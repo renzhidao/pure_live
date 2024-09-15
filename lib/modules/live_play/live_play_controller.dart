2b0a18240e2cfd74dad96a7ab06b52a28af39a9d
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-09-15 22:48:01 +0800

    fix

--- lib/modules/live_play/live_play_controller.dart
+++ lib/modules/live_play/live_play_controller.dart
@@ -477,6 +477,9 @@ class LivePlayController extends StateController {
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
       };
     }
+
+    videoController?.dispose();
+
     videoController = VideoController(
       playerKey: playerKey,
       room: detail.value!,
