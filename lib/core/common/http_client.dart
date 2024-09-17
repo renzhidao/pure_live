652f91ad13f622a410b9f5b4a68f540127e4eb76
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-09-17 08:33:58 +0800

    fix

--- lib/core/common/http_client.dart
+++ lib/core/common/http_client.dart
@@ -18,7 +18,7 @@ class HttpClient {
   HttpClient() {
     dio = Dio(
       BaseOptions(
-        connectTimeout: const Duration(seconds: 20),
+        connectTimeout: const Duration(seconds: 5),
         receiveTimeout: const Duration(seconds: 20),
         sendTimeout: const Duration(seconds: 20),
       ),
