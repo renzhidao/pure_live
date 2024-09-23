a72a3e73b60901895b495d6cda75df6865d034b8
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-09-23 21:37:35 +0800

    fix 配置请求缓存

--- lib/core/common/http_client.dart
+++ lib/core/common/http_client.dart
@@ -17,39 +17,40 @@ class HttpClient {
     return _httpUtil!;
   }
 
+  static getCacheStore() async {
+    var dir = await getTemporaryDirectory();
+    return DbCacheStore(databasePath: dir.path, logStatements: true);
+  }
+
+  static CacheOptions cacheOptions = CacheOptions(
+    // A default store is required for interceptor.
+    store: getCacheStore(),
+
+    // All subsequent fields are optional.
+
+    // Default.
+    policy: CachePolicy.request,
+    // Returns a cached response on error but for statuses 401 & 403.
+    // Also allows to return a cached response on network errors (e.g. offline usage).
+    // Defaults to [null].
+    hitCacheOnErrorExcept: const [401, 403],
+    // Overrides any HTTP directive to delete entry past this duration.
+    // Useful only when origin server has no cache config or custom behaviour is desired.
+    // Defaults to [null].
+    maxStale: const Duration(minutes: 2),
+    // Default. Allows 3 cache sets and ease cleanup.
+    priority: CachePriority.normal,
+    // Default. Body and headers encryption with your own algorithm.
+    cipher: null,
+    // Default. Key builder to retrieve requests.
+    keyBuilder: CacheOptions.defaultCacheKeyBuilder,
+    // Default. Allows to cache POST requests.
+    // Overriding [keyBuilder] is strongly recommended when [true].
+    allowPostMethod: false,
+  );
+
   late Dio dio;
   HttpClient() {
-    late CacheStore cacheStore;
-    getTemporaryDirectory().then((dir) {
-      cacheStore = DbCacheStore(databasePath: dir.path, logStatements: true);
-    });
-
-    final cacheOptions = CacheOptions(
-      // A default store is required for interceptor.
-      store: cacheStore,
-
-      // All subsequent fields are optional.
-
-      // Default.
-      policy: CachePolicy.request,
-      // Returns a cached response on error but for statuses 401 & 403.
-      // Also allows to return a cached response on network errors (e.g. offline usage).
-      // Defaults to [null].
-      hitCacheOnErrorExcept: const [401, 403],
-      // Overrides any HTTP directive to delete entry past this duration.
-      // Useful only when origin server has no cache config or custom behaviour is desired.
-      // Defaults to [null].
-      maxStale: const Duration(minutes: 2),
-      // Default. Allows 3 cache sets and ease cleanup.
-      priority: CachePriority.normal,
-      // Default. Body and headers encryption with your own algorithm.
-      cipher: null,
-      // Default. Key builder to retrieve requests.
-      keyBuilder: CacheOptions.defaultCacheKeyBuilder,
-      // Default. Allows to cache POST requests.
-      // Overriding [keyBuilder] is strongly recommended when [true].
-      allowPostMethod: false,
-    );
 
     dio = Dio(
       BaseOptions(
