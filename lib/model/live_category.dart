300732edc793a098829e8d410b3a211215f920b1
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-09-25 22:47:14 +0800

    fix cache LiveCategory

--- lib/model/live_category.dart
+++ lib/model/live_category.dart
@@ -3,25 +3,33 @@ import 'dart:convert';
 import 'package:pure_live/common/models/index.dart';
 
 class LiveCategory {
-  final String name;
-  final String id;
-  final List<LiveArea> children;
+  late String name;
+  late String id;
+  late List<LiveArea> children;
+
   LiveCategory({
     required this.id,
     required this.name,
     required this.children,
   });
 
-  LiveCategory.fromJson(Map<String, dynamic> json)
-      : name = json['name'] ?? '',
-        id = json['id'] ?? '',
-        children = jsonDecode(json['children'] ?? "[]") as List<LiveArea>;
+  LiveCategory.fromJson(Map<String, dynamic> json) {
+    name = json['name'] ?? '';
+    id = json['id'] ?? '';
+    children = <LiveArea>[];
+    var childrenList = json['children'] as List;
+    if (childrenList.isNotEmpty) {
+      for (var i in childrenList) {
+        children.add(LiveArea.fromJson(i));
+      }
+    }
+  }
 
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
       'name': name,
       'id': id,
-      'children': jsonEncode(children),
+      'children': children,
     };
   }
 
