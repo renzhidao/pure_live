27bee9b84e53181c9815d3606d1a7a51613590b6
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-09-25 20:07:12 +0800

    fix LiveCategory

--- lib/plugins/cache_to_file.dart
+++ lib/plugins/cache_to_file.dart
@@ -4,18 +4,18 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 
 class CustomCache {
-
   static CustomCache get instance {
     return CustomCache();
   }
+
   final String cacheDir;
 
-  CustomCache({this.cacheDir = 'cache'});
+  CustomCache({this.cacheDir = 'cache_file'});
 
   Future<void> setCache(String key, dynamic value) async {
     final file = await _getFile(key);
     final json = jsonEncode(value);
-    file.writeAsString(json);
+    await file.writeAsString(json);
   }
 
   Future<bool> isExistCache<T>(String key) async {
@@ -33,8 +33,8 @@ class CustomCache {
   }
 
   Future<File> _getFile(String key) async {
-    // var baseDir = await getTemporaryDirectory();
-    await Directory(cacheDir).create(recursive: true);
+    var baseDir = await getTemporaryDirectory();
+    await Directory('${baseDir.path}/$cacheDir').create(recursive: true);
     return File('$cacheDir/$key');
   }
 }
