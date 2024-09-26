e0aef0bba9bd8ea6426b2c196f558e40544bc259
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-09-26 19:52:20 +0800

    fix bug

--- lib/modules/live_play/live_play_page.dart
+++ lib/modules/live_play/live_play_page.dart
@@ -62,7 +62,7 @@ class LivePlayPage extends GetView<LivePlayController> {
               const SizedBox(width: 5),
               if (controller.detail.value != null)
                 Text(
-                  "${Sites.of(controller.currentPlayRoom.value.platform!).name}${controller.currentPlayRoom.value.area == null || controller.currentPlayRoom.value.area != '' ? '' : "/${controller.detail.value!.area}"}",
+                  "${Sites.of(controller.currentPlayRoom.value.platform!).name}${controller.currentPlayRoom.value.area == null || controller.currentPlayRoom.value.area != '' ? '' : "/${controller.currentPlayRoom.value.area}"}",
                   style: Theme.of(Get.context!)
                       .textTheme
                       .bodySmall
