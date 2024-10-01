fde26dd73a9c74a79d20a2d59bb5510af3743281
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-10-01 12:44:25 +0800

    fix 去除视频图片

--- lib/modules/live_play/widgets/video_player/video_player.dart
+++ lib/modules/live_play/widgets/video_player/video_player.dart
@@ -6,7 +6,6 @@ import 'package:media_kit_video/media_kit_video.dart' as media_kit_video;
 import 'package:pure_live/common/index.dart';
 import 'package:pure_live/modules/live_play/widgets/video_player/video_controller.dart';
 import 'package:pure_live/modules/live_play/widgets/video_player/video_controller_panel.dart';
-import 'package:pure_live/plugins/cache_network.dart';
 
 class VideoPlayer extends StatefulWidget {
   final VideoController controller;
@@ -39,11 +38,12 @@ class _VideoPlayerState extends State<VideoPlayer> {
     /// 使用 mpv 解码
     if (isDesktop || widget.controller.videoPlayerIndex == 4) {
       return Stack(children: [
-        Obx(() => media_kit_video.Video(
+        Obx(() =>
+            media_kit_video.Video(
               key: widget.controller.key,
               controller: widget.controller.mediaPlayerController,
               pauseUponEnteringBackgroundMode:
-                  !widget.controller.settings.enableBackgroundPlay.value,
+              !widget.controller.settings.enableBackgroundPlay.value,
               // 进入背景模式时暂停
               resumeUponEnteringForegroundMode: true,
               // 进入前景模式后恢复
@@ -55,63 +55,60 @@ class _VideoPlayerState extends State<VideoPlayer> {
             )),
 
         /// 封面
-        Obx(() => Visibility(
-            visible: !widget.controller.mediaPlayerControllerInitialized.value,
-            child: Card(
-              elevation: 0,
-              margin: const EdgeInsets.all(0),
-              shape:
-                  const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-              clipBehavior: Clip.antiAlias,
-              color: Get.theme.focusColor,
-              child: CacheNetWorkUtils.getCacheImageV2(widget.controller.room.cover!),
-            ))),
+        // Obx(() => Visibility(
+        //     visible: !widget.controller.mediaPlayerControllerInitialized.value,
+        //     child: Card(
+        //       elevation: 0,
+        //       margin: const EdgeInsets.all(0),
+        //       shape:
+        //           const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        //       clipBehavior: Clip.antiAlias,
+        //       color: Get.theme.focusColor,
+        //       child: CacheNetWorkUtils.getCacheImageV2(widget.controller.room.cover!),
+        //     ))),
 
         /// 视频加载中
         Center(
           child: // 中间
-              StreamBuilder(
+          StreamBuilder(
             stream:
-                widget.controller.mediaPlayerController.player.stream.buffering,
+            widget.controller.mediaPlayerController.player.stream.buffering,
             initialData:
-                widget.controller.mediaPlayerController.player.state.buffering,
-            builder: (_, s) => Visibility(
-              visible: s.data ?? false,
-              child: const Center(
-                child: CircularProgressIndicator(),
-              ),
-            ),
+            widget.controller.mediaPlayerController.player.state.buffering,
+            builder: (_, s) =>
+                Visibility(
+                  visible: s.data ?? false,
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
           ),
         ),
       ]);
     } else {
-      return Stack(children: [
-        Obx(() {
-          if (widget.controller.mediaPlayerControllerInitialized.value) {
-            return Chewie(
-              controller: widget.controller.chewieController,
-            );
-          }
-          return Card(
-            elevation: 0,
-            margin: const EdgeInsets.all(0),
-            shape:
-                const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-            clipBehavior: Clip.antiAlias,
-            color: Get.theme.focusColor,
-            child: CacheNetWorkUtils.getCacheImageV2(widget.controller.room.cover!),
-          );
-        }),
+      return Obx(() =>
+          Stack(children: [
+            if (widget.controller.mediaPlayerControllerInitialized.value)
+              Chewie(
+                controller: widget.controller.chewieController,
+              ),
+            // return Card(
+            //   elevation: 0,
+            //   margin: const EdgeInsets.all(0),
+            //   shape:
+            //       const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+            //   clipBehavior: Clip.antiAlias,
+            //   color: Get.theme.focusColor,
+            //   child: CacheNetWorkUtils.getCacheImageV2(widget.controller.room.cover!),
+            //
 
-        /// 视频加载中
-        Obx(
-          () => Visibility(
-              visible: !widget.controller.isBuffering.value,
-              child: const Center(
-                child: CircularProgressIndicator(),
-              )),
-        )
-      ]);
+            /// 视频加载中
+            Visibility(
+                visible: !widget.controller.isBuffering.value,
+                child: const Center(
+                  child: CircularProgressIndicator(),
+                )),
+          ]));
     }
   }
 }
