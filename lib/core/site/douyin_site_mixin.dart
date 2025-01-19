3753e36b3fd4adea50c889a9168defb360e39df9
HDYOU <32186506+HDYOU@users.noreply.github.com>
2025-01-20 07:43:51 +0800

    fix bug

--- lib/core/site/douyin_site_mixin.dart
+++ lib/core/site/douyin_site_mixin.dart
@@ -1,4 +1,5 @@
 import 'package:pure_live/common/models/live_room.dart';
+import 'package:pure_live/core/common/core_log.dart';
 import 'package:pure_live/core/danmaku/douyin_danmaku.dart';
 import 'package:pure_live/core/interface/live_site_mixin.dart';
 
@@ -6,9 +7,10 @@ mixin DouyinSiteMixin on SiteAccount, SiteVideoHeaders, SiteOpen {
   @override
   String getJumpToNativeUrl(LiveRoom liveRoom) {
     try {
-      var args = liveRoom.data as DouyinDanmakuArgs;
+      var args = liveRoom.danmakuData as DouyinDanmakuArgs;
       return "snssdk1128://webcast_room?room_id=${args.roomId}";
     } catch (e) {
+      CoreLog.w("$e");
       return "";
     }
   }
@@ -16,9 +18,10 @@ mixin DouyinSiteMixin on SiteAccount, SiteVideoHeaders, SiteOpen {
   @override
   String getJumpToWebUrl(LiveRoom liveRoom) {
     try {
-      var args = liveRoom.data as DouyinDanmakuArgs;
+      var args = liveRoom.danmakuData as DouyinDanmakuArgs;
       return "https://live.douyin.com/${args.webRid}";
     } catch (e) {
+      CoreLog.w("$e");
       return "";
     }
   }
