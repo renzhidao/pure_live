ee0b180e513506c018fd0629481d110582363863
HDYOU <32186506+HDYOU@users.noreply.github.com>
2025-01-19 20:34:09 +0800

    fix 虎牙播放中断

--- lib/modules/live_play/live_play_controller.dart
+++ lib/modules/live_play/live_play_controller.dart
@@ -738,8 +738,9 @@ class LivePlayController extends StateController {
       };
     } else if (currentSite.id == Sites.huyaSite) {
       headers = {
-        "Referer": "https://m.huya.com",
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 Edg/130.0.0.0"
+        // "Referer": "https://m.huya.com",
+        // "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 Edg/130.0.0.0"
+        "User-Agent": "HYSDK(Windows, 21000308)"
       };
     }
     return headers;
