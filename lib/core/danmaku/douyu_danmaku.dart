f3c868eb9f507935bd39bb06bde4a8b93ba2615b
HDYOU <32186506+HDYOU@users.noreply.github.com>
2025-01-19 11:10:59 +0800

    fix 斗鱼 高能弹幕

--- lib/core/danmaku/douyu_danmaku.dart
+++ lib/core/danmaku/douyu_danmaku.dart
@@ -94,6 +94,23 @@ class DouyuDanmaku implements LiveDanmaku {
         );
 
         onMessage?.call(liveMsg);
+      } else if (type == "comm_chatmsg") {
+        // 高能弹幕
+        // {"type":"comm_chatmsg","tick":null,"res":null,"cmdEnum":null,"cmd":"comm_chatmsg","vrid":"1856171486511906816","btype":"voiceDanmu","chatmsg":{"nn":"King彡吖西","level":"14","type":"chatmsg","rid":"1126960","gag":"0","uid":"16751345","txt":"c桑又在赞elo了，上分给你玩明白了","hidenick":"0","nc":"0","ic":["avatar","016","75","13","45_avatar"],"nl":"0","tbid":"0","tbl":"0","tbvip":"0"},"range":"2","cprice":"3000","cmgType":"1","rid":"1126960","gbtemp":"2","uid":"16751345","crealPrice":"3000","cet":"60","now":"1731380814042","csuperScreen":"0","danmucr":"1"}
+        // bnn 粉丝牌 bl 牌子等级 brid 牌子对应的直播间
+        var chatMsg = jsonData["chatmsg"] ?? {};
+        var col = int.tryParse(chatMsg["col"].toString()) ?? 0;
+        var liveMsg = LiveMessage(
+          type: LiveMessageType.superChat,
+          userName: chatMsg["nn"].toString(),
+          message: chatMsg["txt"].toString(),
+          userLevel: chatMsg["level"].toString(),
+          fansName: chatMsg["bnn"].toString(),
+          fansLevel: chatMsg["bl"].toString(),
+          color: getColor(col),
+        );
+
+        onMessage?.call(liveMsg);
       }
     } catch (e) {
       CoreLog.error(e);
