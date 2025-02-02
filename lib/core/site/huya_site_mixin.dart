d53a1e8352e96d1b6ee4d0fcb9ca133ed7ec0a97
HDYOU <32186506+HDYOU@users.noreply.github.com>
2025-02-02 11:42:41 +0800

    fix 虎牙断流

--- lib/core/site/huya_site_mixin.dart
+++ lib/core/site/huya_site_mixin.dart
@@ -190,10 +190,11 @@ mixin HuyaSiteMixin on SiteAccount, SiteVideoHeaders, SiteOpen {
 
   @override
   Map<String, String> getVideoHeaders() {
+    var currentTs = DateTime.now().millisecondsSinceEpoch ~/ 1000;
     return {
       // "Referer": "https://m.huya.com",
       // "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 Edg/130.0.0.0"
-      "User-Agent": "HYSDK(Windows, 21000308)",
+      "User-Agent": "HYSDK(Windows, $currentTs)",
     };
   }
 
