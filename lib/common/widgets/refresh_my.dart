1be36e5ce63107d5968342611a8aee22b3f198e5
HDYOU <32186506+HDYOU@users.noreply.github.com>
2025-05-07 19:05:08 +0800

    fix bug5

--- lib/common/widgets/refresh_my.dart
+++ lib/common/widgets/refresh_my.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:easy_debounce/easy_throttle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:get/get.dart';
 
 import '../base/base_controller.dart';
 import '../l10n/generated/l10n.dart';
@@ -56,12 +55,11 @@ class _RefreshMyState extends State<RefreshMy> with AutomaticKeepAliveClientMixi
 
   @override
   Widget build(BuildContext context) {
-
     var itemBuilder = widget.itemBuilder ?? (context, index) => RoomCard(room: widget.pageController.list[index], dense: true);
 
     return LayoutBuilder(builder: (context, constraint) {
       final crossAxisCount = RefreshGridUtil.getCrossAxisCount(constraint);
-      return Obx(() => RefreshIndicator(
+      return RefreshIndicator(
           displacement: 10.0,
           edgeOffset: 10.0,
           onRefresh: () async {
@@ -69,38 +67,48 @@ class _RefreshMyState extends State<RefreshMy> with AutomaticKeepAliveClientMixi
             await Future.delayed(const Duration(milliseconds: 300));
           },
           child: Stack(children: [
-            if (widget.pageController.list.isNotEmpty)
-              CustomScrollView(
-                cacheExtent: 3500,
-                controller: scrollController,
-                physics: const AlwaysScrollableScrollPhysics(),
-                slivers: [
-                  SliverPadding(
-                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                      sliver: GradUtil.contentGrid(
-                        /// 缓存数目， 减少卡顿
-                        cacheExtent: 3500,
+            StreamBuilder(
+                initialData: [],
+                stream: widget.pageController.list.stream,
+                builder: (s, d) {
+                  if (widget.pageController.list.isEmpty) {
+                    return EmptyView(
+                      icon: Icons.live_tv_rounded,
+                      title: S.current.empty_live_title,
+                      subtitle: S.current.empty_live_subtitle,
+                      boxConstraints: constraint,
+                    );
+                  }
+                  return CustomScrollView(
+                    cacheExtent: 3500,
+                    controller: scrollController,
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    slivers: [
+                      SliverPadding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                          sliver: GradUtil.contentGrid(
+                            /// 缓存数目， 减少卡顿
+                            cacheExtent: 3500,
 
-                        padding: const EdgeInsets.all(5),
-                        controller: widget.pageController.scrollController,
-                        crossAxisCount: crossAxisCount,
-                        itemCount: widget.pageController.list.length,
-                        itemBuilder: itemBuilder,
-                      )),
-                ],
-              ),
-            if (widget.pageController.list.isEmpty)
-              EmptyView(
-                icon: Icons.live_tv_rounded,
-                title: S.current.empty_live_title,
-                subtitle: S.current.empty_live_subtitle,
-                boxConstraints: constraint,
-              ),
-            Visibility(
-              visible: (widget.pageController.loadding.value),
-              child: const AppLoaddingWidget(),
-            ),
-          ])));
+                            padding: const EdgeInsets.all(5),
+                            controller: widget.pageController.scrollController,
+                            crossAxisCount: crossAxisCount,
+                            itemCount: widget.pageController.list.length,
+                            itemBuilder: itemBuilder,
+                          )),
+                    ],
+                  );
+                }),
+            StreamBuilder(
+                initialData: false,
+                stream: widget.pageController.loadding.stream,
+                builder: (s, d) {
+                  return Visibility(
+                    visible: (widget.pageController.loadding.value),
+                    child: const AppLoaddingWidget(),
+                  );
+                }),
+          ]));
     });
   }
 }
