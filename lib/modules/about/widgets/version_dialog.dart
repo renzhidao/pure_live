13893522200a8686edc9898dbfb4cc85de485771
liuchuancong <chuancong@wangdou-hbean.com>
2025-12-01 13:12:05 +0800

    fix(windows upgrade)

--- lib/modules/about/widgets/version_dialog.dart
+++ lib/modules/about/widgets/version_dialog.dart
@@ -1,10 +1,10 @@
+import 'dart:io';
 import 'package:pure_live/common/index.dart';
+import 'package:pure_live/plugins/update.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class NoNewVersionDialog extends StatelessWidget {
-  const NoNewVersionDialog({
-    super.key,
-  });
+  const NoNewVersionDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +30,11 @@ class NewVersionDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final apkUrl =
+        '${VersionUtil.projectUrl}/releases/download/v${VersionUtil.latestVersion}/app-armeabi-v7a-release.apk';
+    final windowsExecutableUrl =
+        '${VersionUtil.projectUrl}/releases/download/v${VersionUtil.latestVersion}/PureLive-windows-x64-setup.exe';
+
     return AlertDialog(
       title: Text(S.of(context).check_update),
       content: Column(
@@ -38,10 +43,7 @@ class NewVersionDialog extends StatelessWidget {
         children: [
           Text(S.of(context).new_version_info(VersionUtil.latestVersion)),
           const SizedBox(height: 20),
-          Text(
-            VersionUtil.latestUpdateLog,
-            style: Theme.of(context).textTheme.bodySmall,
-          ),
+          Text(VersionUtil.latestUpdateLog, style: Theme.of(context).textTheme.bodySmall),
           const SizedBox(height: 10),
           TextButton(
             onPressed: () {
@@ -50,39 +52,51 @@ class NewVersionDialog extends StatelessWidget {
               } else {
                 Navigator.pop(context);
               }
-              launchUrl(
-                Uri.parse('https://www.123pan.com/s/Jucxjv-NwYYd.html'),
-                mode: LaunchMode.externalApplication,
-              );
+              launchUrl(Uri.parse('https://github.com/liuchuancong/pure_live'), mode: LaunchMode.externalApplication);
             },
-            child: const Text('本软件开源免费,国内下载：123云盘'),
-          )
+            child: const Text('本软件开源免费'),
+          ),
         ],
       ),
+      actionsAlignment: MainAxisAlignment.start,
       actions: <Widget>[
-        TextButton(
-          child: Text(S.of(context).cancel),
-          onPressed: () {
-            if (entry != null) {
-              entry!.remove();
-            } else {
-              Navigator.pop(context);
-            }
-          },
-        ),
-        ElevatedButton(
-          child: Text(S.of(context).update),
-          onPressed: () {
-            if (entry != null) {
-              entry!.remove();
-            } else {
-              Navigator.pop(context);
-            }
-            launchUrl(
-              Uri.parse('https://github.com/liuchuancong/pure_live/releases'),
-              mode: LaunchMode.externalApplication,
-            );
-          },
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: Wrap(
+                alignment: WrapAlignment.start,
+                spacing: 10,
+                runSpacing: 10,
+                children: [
+                  ...getMirrorUrls(Platform.isWindows ? windowsExecutableUrl : apkUrl).asMap().entries.map((entry) {
+                    int index = entry.key;
+                    String url = entry.value;
+
+                    return ElevatedButton(
+                      child: Text('下载 ${index + 1}'),
+                      onPressed: () {
+                        downloadAndInstallApk(url);
+                      },
+                    );
+                  }),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 60,
+              child: TextButton(
+                child: Text(S.of(context).cancel),
+                onPressed: () {
+                  if (entry != null) {
+                    entry!.remove();
+                  } else {
+                    Navigator.pop(context);
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ],
     );
