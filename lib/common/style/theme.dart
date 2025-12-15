59d402639c4cd0ee9d717930b90f70061688a4b6
liuchuancong <chuancong@wangdou-hbean.com>
2025-12-15 18:09:53 +0800

    fix(*)

--- lib/common/style/theme.dart
+++ lib/common/style/theme.dart
@@ -6,15 +6,10 @@ class MyTheme {
   ColorScheme? colorScheme;
   String? fontFamily;
 
-  MyTheme({
-    this.primaryColor,
-    this.colorScheme,
-  }) : assert(colorScheme == null || primaryColor == null);
+  MyTheme({this.primaryColor, this.colorScheme}) : assert(colorScheme == null || primaryColor == null);
 
   ThemeData get lightThemeData {
-    if (Platform.isWindows) {
-      fontFamily = 'PingFang';
-    }
+    fontFamily = 'PingFang';
     return ThemeData(
       useMaterial3: true,
       splashFactory: NoSplash.splashFactory,
@@ -28,16 +23,12 @@ class MyTheme {
   }
 
   ThemeData get darkThemeData {
-    if (Platform.isWindows) {
-      fontFamily = 'PingFang';
-    }
+    fontFamily = 'PingFang';
     return ThemeData(
       useMaterial3: true,
       colorSchemeSeed: primaryColor,
       splashFactory: NoSplash.splashFactory,
-      colorScheme: colorScheme?.copyWith(
-        error: const Color.fromARGB(255, 255, 99, 71),
-      ),
+      colorScheme: colorScheme?.copyWith(error: const Color.fromARGB(255, 255, 99, 71)),
       tabBarTheme: const TabBarThemeData(dividerColor: Colors.transparent),
       appBarTheme: const AppBarTheme(scrolledUnderElevation: 0.0, surfaceTintColor: Colors.transparent),
       brightness: Brightness.dark,
